87dc331c4acf8ed900e85936d3c6f5b71b10f242
Lars Greiss <kufikugel@googlemail.com>
2014-12-25 07:03:46 +0000

    HeadsUp: do not show if notification drawer is visible
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
    
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -2172,8 +2172,14 @@ public abstract class BaseStatusBar extends SystemUI implements
                 mContext.getSystemService(Context.INPUT_METHOD_SERVICE);
 
         boolean isIMEShowing = inputMethodManager.isImeShowing();
-//+        boolean keyguardIsShowing = keyguard.isShowingAndNotOccluded()
-//              && keyguard.isInputRestricted();
+        final KeyguardTouchDelegate keyguard = KeyguardTouchDelegate.getInstance(mContext);
+        boolean keyguardIsShowing = keyguard.isShowingAndNotOccluded()
+                && keyguard.isInputRestricted();
+
+        boolean isExpanded = false;
+            if (mStackScroller != null) {
+                isExpanded = mStackScroller.getIsExpanded();
+        }
 
         boolean interrupt = (isFullscreen || (isHighPriority && (isNoisy || hasTicker)))
                 && isAllowed
@@ -2181,7 +2187,8 @@ public abstract class BaseStatusBar extends SystemUI implements
                 && !accessibilityForcesLaunch
                 && mPowerManager.isScreenOn()
     	        && !isIMEShowing
-//              && !keyguardIsShowing;
+                && !keyguardIsShowing;
+                && !isExpanded;
 
         try {
             interrupt = interrupt && !mDreamManager.isDreaming();
@@ -2191,7 +2198,7 @@ public abstract class BaseStatusBar extends SystemUI implements
 
         // its below our threshold priority, we might want to always display
         // notifications from certain apps
-        if (!isHighPriority && !isOngoing && !keyguardIsShowing) {
+        if (!isHighPriority && !isOngoing && !keyguardIsShowing && !isExpanded) {
             // However, we don't want to interrupt if we're in an application that is
             // in Do Not Disturb
             if (!isPackageInDnd(getTopLevelPackage())) {
