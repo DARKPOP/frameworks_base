de2b5a0066edf11187b058aa120753382ecd8d7f
Roman Birg <roman@cyngn.com>
2015-08-12 13:34:03 -0700

    SystemUI: add missed super window calls
    onAttachToWindow() and onDetatchedFromWindow() were added in
    d45c6d4f403a90031411651d57448a1e08309bbf, however they do not call
    through to the super PanelView versions of those methods.
    
    Change-Id: I52b4b76835f2704acf08f4134a31ec6e19fd8e31
    Signed-off-by: Roman Birg <roman@cyngn.com>
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
@@ -365,6 +365,7 @@ public class NotificationPanelView extends PanelView implements
 
     @Override
     public void onAttachedToWindow() {
+        super.onAttachedToWindow();
         mSecureCameraLaunchManager.create();
         mSettingsObserver.observe();
 
@@ -372,6 +373,7 @@ public class NotificationPanelView extends PanelView implements
 
     @Override
     public void onDetachedFromWindow() {
+        super.onDetachedFromWindow();
         mSecureCameraLaunchManager.destroy();
         mSettingsObserver.unobserve();
     }
